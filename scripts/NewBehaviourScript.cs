541ac6235360d8dd76d39a297476bb22272b601c
moki74 <imojovic@gmail.com>
2015-07-14 14:48:54 +0200

    intro promenjen

--- scripts/NewBehaviourScript.cs
+++ scripts/NewBehaviourScript.cs
@@ -1,28 +1,3 @@
-<<<<<<< HEAD
-﻿using UnityEngine;
-using System.Collections;
-
-public class NewBehaviourScript : MonoBehaviour {
-	public UISlider uis;
-
-
-	void Start () {
-
-		//uis.value = 0.2f;
-	}
-	// Use this for initialization
-	void OnMouseDown() {
-		if (uis.value > 0.2)
-						uis.value = 0f;
-				else
-						uis.value = 1f;
-	
-	}
-	
-	// Update is called once per frame
-
-}
-=======
 using UnityEngine;
 using System.Collections;
 
@@ -46,4 +21,4 @@ public class NewBehaviourScript : MonoBehaviour {
 	// Update is called once per frame per
 
 }
->>>>>>> origin/master
+
