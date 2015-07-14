6b29b9b9f082577c93b5722add78a3aba1c48a34
moki74 <imojovic@gmail.com>
2015-07-14 16:37:27 +0200

    zvucnik na meniju

--- scripts/menu/ios_toggle.cs
+++ scripts/menu/ios_toggle.cs
@@ -2,37 +2,44 @@
 using System.Collections;
 
 public class ios_toggle : MonoBehaviour {
-	UISlider uis;
+    UISprite  uis;
+    float value =0;
 	// Use this for initialization
 	void Start () {
 
-		uis = transform.GetComponentInParent<UISlider> ();
-		uis.value = (float) PlayerPrefs.GetInt (uis.name);
+		uis = this.GetComponent<UISprite>();
+		value = (float) PlayerPrefs.GetInt (this.name);
+        if (value > 0f) uis.spriteName="High Volume-100";
+             else uis.spriteName="Mute-100";
 	
 	}
 	
 	// Update is called once per frame
-	void OnClick () {
-		if (uis.value == 0f) {
-						uis.value = 1f;
-						if (uis.name == "muzika"){
-								sound.playMusic = true;					
+	public void OnClick() {
+        Debug.Log("KLIIIIK");
+		if (value == 0f) {
+						value = 1f;
+						if (this.name == "muzika"){
+								sound.playMusic = true;	
+                                 
 
 						}
-						else if (uis.name == "naracija")
+						else if (this.name == "naracija")
 								sound.playSounds  = true;
 						else
 								sound.playEffects = true;
+                         uis.spriteName="High Volume-100";
 				} else {
-						uis.value = 0f;
-						if (uis.name == "muzika"){
+						value = 0f;
+						if (this.name == "muzika"){
 							sound.playMusic = false;
 							sound.stopMusic = true;
 						}
-						else if (uis.name == "naracija")
+						else if (this.name == "naracija")
 							sound.playSounds = false;
 						else
 							sound.playEffects = false;
+                         uis.spriteName="Mute-100";
 
 				}
 		menuManager.snimi ();
